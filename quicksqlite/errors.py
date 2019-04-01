4a1a0eb3526017047794d99f338ebede9dfcf8ae
JackTEK <jacktek@milabot.xyz>
2019-04-01 21:29:19 +0100

    Update errors.py

--- quicksqlite/errors.py
+++ quicksqlite/errors.py
@@ -7,15 +7,15 @@ class Error(Exception):
 
 class ListenError(Error):
     def __init__(self, message: str):
-        print(message + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
+        print(str(message) + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
 
 class ConnectError(Error):
     def __init__(self, message: str):
-        print(message + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
+        print(str(message) + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
 
 class DBError(Error):
     def __init__(self, message: str):
-        if re.search("database is locked", message, re.IGNORECASE):
+        if re.search("database is locked", str(message), re.IGNORECASE):
             log.warn("Database is locked, the request has been stacked onto the request handler.")
 
-        print(message + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
+        print(str(message) + " For support, check https://discord.gg/gDcqBJJ", file=sys.stderr)
